9e824ee277750dd2849b0a83cda99489a0a80fd9
EMRAN YONAS YIMER <emranyonas602@gmail.com>
2023-09-21 04:21:30 -0500

    new feature added to home

---
+++ src/data/SamplesImage.jsx
@@ -0,0 +1,17 @@
+import SampleOne from '../assets/SampleHomeImages/SampleOne.jpg'
+import SampleTwo from '../assets/SampleHomeImages/SampleTwo.jpg'
+import SampleThree from '../assets/SampleHomeImages/SampleThree.jpg'
+import SampleFour from '../assets/SampleHomeImages/SampleFour.jpg'
+import SampleFive from '../assets/SampleHomeImages/SampleFive.jpg'
+import SampleSix from '../assets/SampleHomeImages/SampleSix.jpg'
+import SampleSeven from '../assets/SampleHomeImages/SampleSeven.jpg'
+import SampleEight from '../assets/SampleHomeImages/SampleEight.jpg'
+import SampleNine from '../assets/SampleHomeImages/SampleNine.jpg'
+import SampleTen from '../assets/SampleHomeImages/SampleTen.jpg'
+import SampleEleven from '../assets/SampleHomeImages/SampleEleven.jpg'
+
+const HomeImageSamples = [
+    SampleOne,SampleTwo,SampleThree,SampleFour,SampleFive,SampleSix,SampleSeven,SampleEight,SampleNine,SampleTen,SampleEleven
+]
+
+export default HomeImageSamples
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import data from '../data/data'
+import HomeImageSamples from '../data/SamplesImage'
 import Reason from '../Components/Reason'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+import { AiOutlineArrowRight } from 'react-icons/ai'
 
 function Home() {
 
+    const handleLeftArrowClick = () =>{
+      if(index > 0){
+        setIndex(prev => prev - 1)
+      }
+      console(index)
+    }
+    const handleRightArrowClick = () =>{
+      if(index < 10 ){
+        setIndex(prev => prev + 1)
+      }else{
+        return
+      }
+      console.log(index)
+    }
 
+    const [index,setIndex] = React.useState(0)
     const Reasons = data.map(item => <Reason key={item.id} imgURL={item.imgURL} altText={item.altText} title={item.title} description={item.description}/>)
 
 
@@ -19,6 +37,15 @@ function Home() {
         <section className='Home-page-2'>
             <h3 className='Home-page-2-title'>Reasons why you shop from Us</h3>
             {Reasons}
+        </section>
+
+        <section className='sample-image-container'>
+          <h2 className='sample-image-title'>Sample Images</h2>
+          <section className='sample-images'>
+            <div onClick={handleLeftArrowClick} className={`left-arrow ${index === 0 && 'red-blurr'}`}><AiOutlineArrowLeft/></div>
+            <img className='Home-sample-image' src={HomeImageSamples[index]}/>
+            <div onClick={handleRightArrowClick} className={`right-arrow ${index === 10 && 'red-blurr'} `}><AiOutlineArrowRight/></div>
+          </section>
         </section>
     </div>
   )
